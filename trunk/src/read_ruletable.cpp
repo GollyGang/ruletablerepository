ef03ebf861b1c706de08c009049ef20628cad11e
tim.hutton <tim.hutton@c6cce94a-ba76-11dd-b040-2d45f096adfe>
2009-02-07 18:51:28 +0000

    See read_ruletable.cpp for how to add rule tables to your CA project. See make-ruletable.cpp for how to create a rule table from a C++ implementation of your CA.

---
+++ trunk/src/read_ruletable.cpp
@@ -0,0 +1,58 @@
+/*
+
+This file is part of The Rule Table Repository.
+Copyright (C) 2009 Tim Hutton.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+/*
+
+Demonstrates how to read rule tables from file for your CA project.
+
+To compile:
+g++ read_ruletable.cpp ruletable.cpp -o read_ruletable
+or in Microsoft Visual Studio, add files to an empty CLR project.
+
+Contact: Tim Hutton <tim.hutton@gmail.com>
+
+*/
+
+#include "ruletable.h"
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   // parameters for use:
+   const string input_filename = "wireworld.table";
+
+   ruletable wireworld_rules;
+
+   cout << "Reading from " << input_filename << "...";
+   string returned = wireworld_rules.ReadFromFile(input_filename);
+   if(returned.empty())
+   {
+      cout << "ok.\n";
+      cout << "rule table has " << wireworld_rules.GetNumStates() << " states.\n";
+      cout << "example transition:\n";
+      cout << "0,0,0,0,0,0,0,0,0 -> " << wireworld_rules.slowcalc(0,0,0,0,0,0,0,0,0) << endl;
+   }
+   else
+      cout << "error!\n" << returned << endl;
+}
---
+++ trunk/src/ruletable.h
@@ -0,0 +1,66 @@
+                        /*** /
+
+This file is part of Golly, a Game of Life Simulator.
+Copyright (C) 2008 Andrew Trevorrow and Tomas Rokicki.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ Web site:  http://sourceforge.net/projects/golly
+ Authors:   rokicki@gmail.com  andrew@trevorrow.com
+
+                        / ***/
+
+// Adapted from Golly's ruletable_algo.h
+
+#include <string>
+#include <vector>
+
+typedef int state; 
+
+/// A class that reads CA rule tables from file
+class ruletable
+{
+
+public:
+
+   /// return string is error description if failed, else empty
+   std::string ReadFromFile(const std::string& filename);
+
+   /// how many states does this ruleset define?
+   int GetNumStates() { return n_states; }
+
+   /// this is the update function: from these inputs, what is the output state
+   /// (for von Neumann neighborhoods, pass any value for the unused inputs)
+   state slowcalc(state nw, state n, state ne, state w, state c,
+                state e, state sw, state s, state se);
+
+protected:
+
+   unsigned int n_states;
+   enum TNeighborhood { vonNeumann, Moore } neighborhood; // (hopefully will support more in future)
+   enum TSymmetry { none, rotate4, rotate8, reflect, rotate4reflect, rotate8reflect } symmetries;
+
+   // we use a lookup table to match inputs to outputs:
+   typedef unsigned long long int TBits; 
+   // (we can use unsigned int if we hit portability issues (not much slower))
+
+   std::vector< std::vector< std::vector<TBits> > > lut; 
+   // TBits lut[neighbourhood_size][n_states][n_compressed_rules];
+
+   unsigned int n_compressed_rules;
+   std::vector<state> output; // state output[n_rules];
+
+};
+
